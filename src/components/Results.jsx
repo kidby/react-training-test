30333064d7e5cb16bc4c79ca35a4512f28fe4c59
Michelle Kidby <mkidby@intellum.com>
2023-03-28 14:36:47 +0200

    Update Results.jsx
    fix call navigate() in a React.useEffect() error

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom' // if 'cannot resolve symbol' error: https://stackoverflow.com/questions/70031839/cannot-resolve-symbol-routes
-import styles from '../styles/styles.module.css'
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'; // if 'cannot resolve symbol' error: https://stackoverflow.com/questions/70031839/cannot-resolve-symbol-routes
+import styles from '../styles/styles.module.css';
 
 /**
  * Results component to display the last answer and provide an option to start over
- * @returns {JSX.Element} The Results component
+ * @returns {JSX.Element|null} The Results component unless no last answer
  */
 function Results() {
-    const navigate = useNavigate()
-    const answers = useSelector((state) => state.questions.answers)
-    const lastAnswer = Object.values(answers).pop()
+    const navigate = useNavigate();
+    const answers = useSelector((state) => state.questions.answers);
+    const lastAnswer = Object.values(answers).pop();
 
-    return lastAnswer ? (
+    const clickStartOver = () => navigate('/');
+
+    useEffect(() => {
+        if (!lastAnswer) {
+            navigate('/');
+        }
+    }, [lastAnswer, navigate]);
+
+    if (!lastAnswer) {
+        return null;
+    }
+
+    return (
         <div className={styles.questionContainer}>
             <h2>Your answer:</h2>
             <span className={styles.answerText} aria-labelledby="answer-label">
@@ -20,13 +32,13 @@ function Results() {
             </span>
             <button
                 className={styles.optionButton}
-                onClick={() => navigate('/')}
+                onClick={clickStartOver}
                 aria-label="Start Over"
             >
                 Start Over
             </button>
         </div>
-    ) : navigate('/')
+    )
 }
 
 export default Results;
