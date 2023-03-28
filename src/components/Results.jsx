5236c59ae135b02f6ea8ea10766dd5462e83c192
Michelle Kidby <mkidby@intellum.com>
2023-03-28 13:54:21 +0200

    Update Results.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,25 +8,25 @@ import styles from '../styles/styles.module.css'
  * @returns {JSX.Element} The Results component
  */
 function Results() {
-  const navigate = useNavigate()
-  const answers = useSelector((state) => state.questions.answers)
-  const lastAnswer = Object.values(answers).pop()
+    const navigate = useNavigate()
+    const answers = useSelector((state) => state.questions.answers)
+    const lastAnswer = Object.values(answers).pop()
 
-  return (
-      <div className={styles.questionContainer}>
-          <h2>Your answer:</h2>
-          <span className={styles.answerText} aria-labelledby="answer-label">
-              {lastAnswer ? lastAnswer.label : navigate('/')}
-          </span>
-          <button
-              className={styles.optionButton}
-              onClick={() => navigate('/')}
-              aria-label="Start Over"
-          >
-              Start Over
-          </button>
-      </div>
-  )
+    return lastAnswer ? (
+        <div className={styles.questionContainer}>
+            <h2>Your answer:</h2>
+            <span className={styles.answerText} aria-labelledby="answer-label">
+                {lastAnswer.label}
+            </span>
+            <button
+                className={styles.optionButton}
+                onClick={() => navigate('/')}
+                aria-label="Start Over"
+            >
+                Start Over
+            </button>
+        </div>
+    ) : navigate('/')
 }
 
 export default Results;
